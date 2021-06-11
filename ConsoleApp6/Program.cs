f42f2fc26f6f1aa9c13a4f7362ed18e297618904
zabishniy <kzabishniy@gmail.com>
2021-06-11 09:45:24 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Go(object num)
+        {
+            int n = (int)num;
+            if (n <= 0)
+                return;
+            Thread t = new Thread(Go);
+            t.Name = "Thread" + n;
+            t.Start(n - 1);
+            Console.WriteLine(t.Name);
+        }
+        private static void Main(string[] args)
+        {
+            Go(10);
+            Console.ReadKey();
+        }
+    }
+}
